a3d39e96422c5739276445e132434697ba8d3495
Hikaru Shawanda <hshawand@vo-e4.21-school.ru>
2019-12-18 16:32:10 +0300

    Parser core done

--- srcs/parser.c
+++ srcs/parser.c
@@ -1,22 +1,56 @@
-#include "../parser.h"
+#include "parser.h"
 
 /*
 ** functioun to check if find char is in use or just text
 */
 
+char	get_tech_num(char check)
+{
+	char	*base;
+	size_t	i;
+
+	base = " \\;&\"\'()[]{}$~|><*";
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == check)
+			return (i + 1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_get_techline( char *line)
+{
+	char	*tech_str;
+	size_t	i;
+
+	i = 0;
+	if(!(tech_str = (char *)malloc(ft_strlen(line)))) // change to sschmele's xmalloc
+		return (0);
+	while (*line)
+	{
+		tech_str[i] = get_tech_num(*line);
+		line++;
+		i++;
+	}
+	return (tech_str);
+}
+
+/*
 int	ft_backslash_find(char *start, char *str)
 {
-	if (str > start && *(str - 1) != '\')
+	if (str > start && *(str - 1) != '\\')
 		return(0);
 	else
 		return(1);
 }
-
+*/
 /*
 ** Parser function that gets **string from Readline or from argv
 ** and separetes it to blockes to start
 */
-
+/*
 int	ft_parser(char **str)
 {
 	char	**str_tech;
@@ -32,3 +66,4 @@ int	ft_parser(char **str)
 		ft_get_techline(str, str_tech);
 	}
 }
+*/
