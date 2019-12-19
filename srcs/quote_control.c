2f56cbb01ca4fc8d9dea2738de966a069221dd9f
rbednar-vs <rabota.voyt@mail.ru>
2019-12-20 00:14:39 +0300

    udate parcer.c, parcer.h add slice_to_blocks.c

--- srcs/quote_control.c
+++ srcs/quote_control.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   quote_control.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hshawand <hshawand@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rbednar <rbednar@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/18 17:28:46 by hshawand          #+#    #+#             */
-/*   Updated: 2019/12/18 18:15:21 by hshawand         ###   ########.fr       */
+/*   Updated: 2019/12/19 15:32:42 by rbednar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ int		terminate(char mode, char *ptr)
 {
 	if (mode == 1)
 	{
-		if (*ptr == 1 || *ptr == 3 || *ptr == 4 || *ptr == 9 || *ptr == 10 || *ptr == 14 || *ptr == 15 || *ptr == 16 || *ptr == 17 || *ptr == 18)
+		if (*ptr == 1 || *ptr == 3 || *ptr == 4 || *ptr == 9 \
+		|| *ptr == 10 || *ptr == 14 || *ptr == 15 || *ptr == 16 \
+		|| *ptr == 17 || *ptr == 18)
 			*ptr = 0;
 	}
 	else
