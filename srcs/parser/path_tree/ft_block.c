cf8a8f19f7ed15508a206d31a384f564bdbb34f0
rbednar-vs <rabota.voyt@mail.ru>
2020-01-18 22:28:37 +0300

    update

--- srcs/parser/path_tree/ft_block.c
+++ srcs/parser/path_tree/ft_block.c
@@ -6,29 +6,11 @@
 /*   By: rbednar <rbednar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/26 15:36:08 by rbednar           #+#    #+#             */
-/*   Updated: 2020/01/18 20:41:13 by rbednar          ###   ########.fr       */
+/*   Updated: 2020/01/18 22:28:16 by rbednar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/parser.h"
-
-/*
-** Func fills **list by element of tree t_path
-*/
-
-static void		ft_fill_in_order(t_path **root, char **list, size_t *len)
-{
-	if (root != NULL && *root != NULL)
-	{
-		ft_fill_in_order(&((*root)->prev), list, len);
-		if ((*root)->name != NULL)
-		{
-			list[*len] = ft_strdup((*root)->name);
-			(*len)++;
-		}
-		ft_fill_in_order(&((*root)->next), list, len);
-	}
-}
 
 /*
 ** This func needs to fill path in the struct of file
@@ -63,6 +45,24 @@ void			ft_addpath(char *name, t_path **buf)
 	(*buf)->path = ft_strcpy((*buf)->path, dir_path);
 	(*buf)->path = ft_strcat((*buf)->path, (*buf)->name);
 	free(dir_path);
+}
+
+/*
+** Func fills **list by element of tree t_path
+*/
+
+static void		ft_fill_in_order(t_path **root, char **list, size_t *len)
+{
+	if (root != NULL && *root != NULL)
+	{
+		ft_fill_in_order(&((*root)->prev), list, len);
+		if ((*root)->name != NULL)
+		{
+			list[*len] = ft_strdup((*root)->name);
+			(*len)++;
+		}
+		ft_fill_in_order(&((*root)->next), list, len);
+	}
 }
 
 /*
