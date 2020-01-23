e4dde3c8e1fbc891c102bec9a91aa250cf0eff10
Vladlena <vskubish1410@bk.ru>
2020-01-23 18:00:05 +0300

    Variables almost ready

--- srcs/readline42/auto_completion/menu_receipt.c
+++ srcs/readline42/auto_completion/menu_receipt.c
@@ -6,7 +6,7 @@
 /*   By: sschmele <sschmele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 13:17:28 by sschmele          #+#    #+#             */
-/*   Updated: 2020/01/23 16:34:16 by sschmele         ###   ########.fr       */
+/*   Updated: 2020/01/23 17:58:07 by sschmele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,33 @@ char			*path_parse_compl(void)
 char			**get_variables(char *complete, size_t *total, int *max_len)
 {
 	char		**menu = NULL;
+	size_t		i;
+	t_path		*root;
+	char		*tmp;
+	size_t		len;
 
-	printf("variables\n");
+	i = 0;
+	len = ft_strlen(complete);
+	root = NULL;
+	while (g_env[i])
+	{
+		tmp = ft_strndup(g_env[i], ft_strchri(g_env[i], '='));
+		if (ft_strnequ(tmp, complete, len))
+			insert(tmp, &root, total);
+		free(tmp);
+		i++;
+	}
+	i = 0;
+	while (g_shvar[i])
+	{
+		tmp = ft_strndup(g_shvar[i], ft_strchri(g_shvar[i], '='));
+		if (ft_strnequ(tmp, complete, len))
+			insert(tmp, &root, total);
+		free(tmp);
+		i++;
+	}
+	menu = ft_add_block(&root, *total, max_len);
+	ft_path_free(&root);
 	return (menu);
 }
 
