14d0e0fc06d0ac37dee8e43df8fd5521cc5f4b79
rbednar <voyt-sv@mail.ru>
2020-01-23 20:37:11 +0300

    update parser, pipe OK, need to ID of out process

--- srcs/parser/find_spec.c
+++ srcs/parser/find_spec.c
@@ -6,7 +6,7 @@
 /*   By: rbednar <rbednar@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/22 13:04:56 by rbednar           #+#    #+#             */
-/*   Updated: 2020/01/23 16:40:55 by rbednar          ###   ########.fr       */
+/*   Updated: 2020/01/23 20:08:46 by rbednar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,11 @@ t_ltree		*ft_find_logic(t_ltree *block, t_ltree *final, int *buf)
 
 	i = block->start;
 	final->flags = 0;
-	while (i < block->end)
+	while (i <= block->end)
 	{
 		if (g_techline.line[i] == PIPE && g_techline.line[i + 1] == PIPE)
 		{
 			*buf= block->end;
-			block->start = i + 2;
 			block->end = i - 1;
 			block->flags = LOG_OR;
 			return (ft_find_logic(block, final, buf));
