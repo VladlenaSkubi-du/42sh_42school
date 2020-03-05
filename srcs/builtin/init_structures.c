2ff58f9b3ad97a2060ecee9309574fc9dc82462b
Vladlena <vskubish1410@bk.ru>
2020-03-05 20:32:14 +0300

    fc changes

--- srcs/builtin/init_structures.c
+++ srcs/builtin/init_structures.c
@@ -6,23 +6,24 @@
 /*   By: sschmele <sschmele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/05 13:38:16 by sschmele          #+#    #+#             */
-/*   Updated: 2020/03/05 13:40:46 by sschmele         ###   ########.fr       */
+/*   Updated: 2020/03/05 20:30:16 by sschmele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell42.h"
 #include "builtin42.h"
 
-t_btin_fc			init_btin_fc(void)
+t_btin_fc			*init_btin_fc(void)
 {
-	t_btin_fc		fc;
+	t_btin_fc		*fc;
 
-	fc.editor = NULL;
-    fc.arg_e_first = -1;
-	fc.arg_e_last = -1;
-	fc.arg_l_first = -17;
-	fc.arg_l_first = -1;
-	fc.arg_s_comp = NULL;
-	fc.arg_s_first = -1;
+	fc = (t_btin_fc*)ft_xmalloc(sizeof(t_btin_fc));
+	fc->editor = NULL;
+    fc->arg_e_first = -1;
+	fc->arg_e_last = -1;
+	fc->arg_l_first = -17;
+	fc->arg_l_first = -1;
+	fc->arg_s_comp = NULL;
+	fc->arg_s_first = -1;
 	return (fc);
 }
