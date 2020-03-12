efc1b53cb09a67e5f8ec69f7f58a90df3d09af34
rbednar <rbednar@student.21-school.ru>
2020-03-12 22:08:38 +0300

    update buildins_list.h - name of functions that not added correct to not have segfault

--- includes/buildins_list.h
+++ includes/buildins_list.h
@@ -6,16 +6,16 @@
 /*   By: rbednar <rbednar@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/12 19:07:24 by rbednar           #+#    #+#             */
-/*   Updated: 2020/03/12 20:50:27 by rbednar          ###   ########.fr       */
+/*   Updated: 2020/03/12 22:05:02 by rbednar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_LIST_H
 # define BUILTINS_LIST_H
 
-static char		*g_buildins[] = {"alias", "unalias", \
-	"hash", "set", "unset", "export", \
-	"cd", "exit", "echo", "type", "fg", "bg", "jobs", "fc", NULL};
+static char		*g_buildins[] = {"alias_fff", "unalias_fff", \
+	"hash_fff", "set", "unset", "export_fff", \
+	"cd_ff", "exit", "echo_bi" "type_fff", "fg_ff", "bg_ff", "jobs_ff", "fc_ff", NULL};
 
 static int 		(*g_buildins_func[15])() = {NULL, NULL, \
 	NULL, btin_set, btin_unset, NULL, NULL, btin_exit, \
