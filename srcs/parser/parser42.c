1443e0c89c948b9354a098bcae819ce8134f12f4
Vladlena <vskubish1410@bk.ru>
2020-03-16 19:37:20 +0300

    Cast parser off

--- srcs/parser/parser42.c
+++ srcs/parser/parser42.c
@@ -6,7 +6,7 @@
 /*   By: sschmele <sschmele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/23 19:19:56 by rbednar           #+#    #+#             */
-/*   Updated: 2020/03/16 17:49:29 by sschmele         ###   ########.fr       */
+/*   Updated: 2020/03/16 19:37:03 by sschmele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ static int			castrated_parser(void)
 
 int		pars_lex_exec(int tmp)
 {
-	castrated_parser(); //TODO delete
+	// castrated_parser(); //TODO delete
 	if (g_prompt.prompt_func != heredoc_prompt)
 		if (nullify(&g_techline.line, g_cmd_size) == OUT)
 		{
