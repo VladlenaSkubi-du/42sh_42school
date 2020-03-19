e03af495e0715ebe638cbe6aa8b6b58d40f0bd55
rbednar-vs <rabota.voyt@mail.ru>
2020-03-19 15:28:55 +0300

    redirection in buildins update

--- srcs/parser/exec/exec_core.c
+++ srcs/parser/exec/exec_core.c
@@ -168,8 +168,10 @@ int		ft_builtins_check(t_ltree *pos, int flag)
 		{
 			if (flag)
 			{
+				std_save(0);
 				fd_list_process(pos);
 				g_builtins_func[i](pos);
+				std_save(1);
 			}
 			return (i);
 		}
@@ -209,9 +211,9 @@ int		std_save(int mode)
 	}
 	else
 	{
-		dup2(save[0], 0);
-		dup2(save[1], 1);
-		dup2(save[2], 2);
+		dup2(STDIN_FILENO, save[0]);
+		dup2(STDOUT_FILENO, save[1]);
+		dup2(STDERR_FILENO, save[2]);
 	}
 }
 
@@ -227,7 +229,6 @@ int		exec_core(t_ltree *pos)
 	(pos->flags & PIPED_IN) ? (pipe_prev = pipe_next[0]) : 0;
 	if ((pos->flags & PIPED_OUT) && pipe(pipe_next) == -1)
 			return (exec_clean(path, -1));
-	std_save(0);
 	if (ft_builtins_check(pos, 1) == -1)
 	{
 		child_pid = fork();
@@ -245,7 +246,6 @@ int		exec_core(t_ltree *pos)
 	}
 	(pos->flags & PIPED_OUT) ? close(pipe_next[1]) : 0;
 	(pos->flags & PIPED_IN) ? close(pipe_prev) : 0;
-	std_save(1);
 	return (exec_clean(path, WIFEXITED(child_pid) ? \
 	WEXITSTATUS(child_pid) : (-1)));
 }
