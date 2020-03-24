197d3f6170746e94f6be0e1251e207cf03f03b20
kfalia-f <unstableperson@yandex.ru>
2020-03-24 16:10:29 +0300

    echo

--- includes/builtins_list.h
+++ includes/builtins_list.h
@@ -17,11 +17,11 @@
 
 static char		*g_builtins[] = {"alias_fff", "unalias_fff", \
 	"hash_fff", "set", "unset", "export_fff", \
-	"cd", "exit", "echo_bi", "type_fff", "fg_ff", "bg_ff", "jobs_ff", "fc", \
+	"cd", "exit", "echo", "type_fff", "fg_ff", "bg_ff", "jobs_ff", "fc", \
 	"history", NULL};
 
 static int 		(*g_builtins_func[BUILTINS_NUM])() = {NULL, NULL, \
 	NULL, btin_set, btin_unset, NULL, btin_cd, btin_exit, \
-	NULL, NULL, NULL, NULL, NULL, btin_fc, btin_history, NULL};
+	btin_echo, NULL, NULL, NULL, NULL, btin_fc, btin_history, NULL};
 
 #endif
