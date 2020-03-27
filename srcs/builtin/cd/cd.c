f231cbb5910f6f10ea64dd30bded4e3948888633
kfalia-f <unstableperson@yandex.ru>
2020-03-27 13:39:53 +0300

    tabs

--- srcs/builtin/cd/cd.c
+++ srcs/builtin/cd/cd.c
@@ -1,60 +1,78 @@
 #include "shell42.h"
 #include "builtin42.h"
 
-void    ft_change_curpath(char *path, t_cd *flags)
+void	ft_change_curpath(char *path, t_cd *flags)
 {
-    free(flags->curpath);
-    if (!path)
-        flags->curpath = NULL;
-    else
-        flags->curpath = ft_strdup(path);
+	free(flags->curpath);
+	if (!path)
+		flags->curpath = NULL;
+	else
+		flags->curpath = ft_strdup(path);
 }
 
-int     ft_error(char *name, int en)
+char	*ft_cut_name(char *name)
 {
-    char    *tmp;
+	char    *tmp;
+	int		i;
 
-    tmp = ft_strdup("cd: ");
-    if (name) 
-        tmp = ft_strrejoin(tmp, name);
-    //ft_putstr_fd("cd", 2);
-    if (en == 1)
-        tmp = ft_strrejoin(tmp, ": string not in pwd");
-    else if (en == 2)
-        tmp = ft_strrejoin(tmp, ": no such file or directory");
-    else if (en == 3)
-        tmp = ft_strrejoin(tmp, ": permission denied");
-    else if (en == 4)
-        tmp = ft_strrejoin(tmp, ": not a directory");
-    else if (en == 5)
-        tmp = ft_strrejoin(tmp, "too many arguments");
-    else if (en == 6)
-        tmp = ft_strrejoin(tmp, "OLDPWD not set");
-    error_handler(VARIABLE_ERROR | (ERR_CD << 9), tmp);
-    free(tmp);
+	i = ft_strlen(name) - 1;
+	printf("name = %s\n", name);
+	while (name[i] && name[i] != '/')
+		i--;
+	i++;
+	tmp = ft_strdup(name + i);
+	printf("tmp = %s\n", tmp);
+	return (tmp);
+}
+
+int		ft_error(char *name, int en)
+{
+    char	*tmp;
+    char	*new_name;
+
+	tmp = ft_strdup("cd: ");
+	new_name = ft_cut_name(name);
+	if (name) 
+		tmp = ft_strrejoin(tmp, new_name);
+	//ft_putstr_fd("cd", 2);
+	if (en == 1)
+		tmp = ft_strrejoin(tmp, ": string not in pwd");
+	else if (en == 2)
+		tmp = ft_strrejoin(tmp, ": no such file or directory");
+	else if (en == 3)
+		tmp = ft_strrejoin(tmp, ": permission denied");
+	else if (en == 4)
+		tmp = ft_strrejoin(tmp, ": not a directory");
+	else if (en == 5)
+		tmp = ft_strrejoin(tmp, "too many arguments");
+	else if (en == 6)
+		tmp = ft_strrejoin(tmp, "OLDPWD not set");
+	error_handler(VARIABLE_ERROR | (ERR_CD << 9), tmp);
+	free(tmp);
+	free(new_name);
     return (1);
 }
 
-int     btin_cd(t_ltree *pos)
+int		btin_cd(t_ltree *pos)
 {
-    int     i;
-    t_cd    *flags;
+    int		i;
+    t_cd	*flags;
 
-    flags = ft_xmalloc(sizeof(t_cd *));
-    i = ft_cd_flags(pos->ar_v, flags);
-    if (ft_valid_cd(pos->ar_v, i))
-    {
-        free(flags->curpath);
-        free(flags);
-        return (1);
-    }
-    if (ft_cd_pars(pos->ar_v[i], g_env, flags))
-    {
-        free(flags->curpath);
-        free(flags);
-        return (1);
-    }
-        free(flags->curpath);
-    free(flags);
-    return (0);
+	flags = ft_xmalloc(sizeof(t_cd *));
+	i = ft_cd_flags(pos->ar_v, flags);
+	if (ft_valid_cd(pos->ar_v, i))
+	{
+		free(flags->curpath);
+		free(flags);
+		return (1);
+	}
+	if (ft_cd_pars(pos->ar_v[i], g_env, flags))
+	{
+		free(flags->curpath);
+		free(flags);
+		return (1);
+	}
+	free(flags->curpath);
+	free(flags);
+	return (0);
 }
--- srcs/builtin/cd/cd_new_path.c
+++ srcs/builtin/cd/cd_new_path.c
@@ -1,77 +1,77 @@
 #include <shell42.h>
 #include <builtin42.h>
 
-void    ft_del_prev(char **arr, int i)
+void	ft_del_prev(char **arr, int i)
 {
-    while (i > 0)
-    {
-        if (ft_strcmp(arr[i], ".") && ft_strcmp(arr[i], ".."))
-            {
-                free(arr[i]);
-                arr[i] = ft_strdup(".");
-                break ;
-            }
-        i--;
-    }
+	while (i > 0)
+	{
+		if (ft_strcmp(arr[i], ".") && ft_strcmp(arr[i], ".."))
+		{
+			free(arr[i]);
+			arr[i] = ft_strdup(".");
+			break ;
+		}
+		i--;
+	}
 }
 
-void    ft_del_dots(char **arr)
+void	ft_del_dots(char **arr)
 {
-    int     i;
+    int		i;
 
-    i = 0;
-    while (arr[i])
-    {
-        if (ft_strcmp(arr[i], "..") == 0)
-            ft_del_prev(arr, i);
-        i++;
-    }
+	i = 0;
+	while (arr[i])
+	{
+		if (ft_strcmp(arr[i], "..") == 0)
+			ft_del_prev(arr, i);
+		i++;
+	}
 }
 
-char    *ft_new_from_arr(char **arr)
+char	*ft_new_from_arr(char **arr)
 {
-    int     i;
-    char    *tmp;
+    int		i;
+    char	*tmp;
 
-    i = 0;
-    tmp = ft_memalloc(1);
-    ft_del_dots(arr);
-    while (arr[i])
-    {
-        if (ft_strcmp(arr[i], ".") && ft_strcmp(arr[i], ".."))
-        {
-            tmp = ft_strrejoin(tmp, "/");
-            tmp = ft_strrejoin(tmp, arr[i]);
-        }
-        i++;
-    }
-    return (tmp);
+	i = 0;
+	tmp = ft_memalloc(1);
+	ft_del_dots(arr);
+	while (arr[i])
+	{
+		if (ft_strcmp(arr[i], ".") && ft_strcmp(arr[i], ".."))
+		{
+			tmp = ft_strrejoin(tmp, "/");
+			tmp = ft_strrejoin(tmp, arr[i]);
+		}
+		i++;
+	}
+	return (tmp);
 }
 
-char    *ft_join(char *path, char **env)
+char	*ft_join(char *path, char **env)
 {
-    size_t  i;
-    size_t  j;
-    char    *tmp;
+	size_t	i;
+	size_t	j;
+	char	*tmp;
 
-    i = find_in_variables(env, &j, "PWD");
-    tmp = ft_strjoin(env[i] + j, "/");
-    tmp = ft_strrejoin(tmp, path);
-    return (tmp);
+	i = find_in_variables(env, &j, "PWD");
+	tmp = ft_strjoin(env[i] + j, "/");
+	tmp = ft_strrejoin(tmp, path);
+	return (tmp);
 }
 
-char    *ft_new_path(char *path, char **env)
+char	*ft_new_path(char *path, char **env)
 {
-    char    *new_path;
-    char    *tmp;
-    char    **arr;
+	char	*new_path;
+	char	*tmp;
+	char	**arr;
 
-    tmp = ft_join(path, env);
-    arr = ft_strsplit(tmp, '/');
-    free(tmp);
-    new_path = ft_new_from_arr(arr);
-    //printf("RES = %s\n", new_path);
-    ft_arrdel(arr);
-    return (new_path);
+	tmp = ft_join(path, env);
+	arr = ft_strsplit(tmp, '/');
+	free(tmp);
+	new_path = ft_new_from_arr(arr);
+	//printf("RES = %s\n", new_path);
+	ft_arrdel(arr);
+	return (new_path);
 }
 
