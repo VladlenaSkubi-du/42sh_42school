f4fcfea38bd166fa4b9893538659cb9088526ba0
Владлена Скубиш <vladlenaskubis@MacBook-AirVlada.local>
2020-03-27 13:38:36 +0300

    errors for commands

--- srcs/builtin/cd/cd.c
+++ srcs/builtin/cd/cd.c
@@ -3,58 +3,58 @@
 
 void    ft_change_curpath(char *path, t_cd *flags)
 {
-    free(flags->curpath);
-    if (!path)
-        flags->curpath = NULL;
-    else
-        flags->curpath = ft_strdup(path);
+	free(flags->curpath);
+	if (!path)
+		flags->curpath = NULL;
+	else
+		flags->curpath = ft_strdup(path);
 }
 
 int     ft_error(char *name, int en)
 {
-    char    *tmp;
+	char    *tmp;
 
-    tmp = ft_strdup("cd: ");
-    if (name) 
-        tmp = ft_strrejoin(tmp, name);
-    //ft_putstr_fd("cd", 2);
-    if (en == 1)
-        tmp = ft_strrejoin(tmp, ": string not in pwd");
-    else if (en == 2)
-        tmp = ft_strrejoin(tmp, ": no such file or directory");
-    else if (en == 3)
-        tmp = ft_strrejoin(tmp, ": permission denied");
-    else if (en == 4)
-        tmp = ft_strrejoin(tmp, ": not a directory");
-    else if (en == 5)
-        tmp = ft_strrejoin(tmp, "too many arguments");
-    else if (en == 6)
-        tmp = ft_strrejoin(tmp, "OLDPWD not set");
-    error_handler(VARIABLE_ERROR | (ERR_CD << 9), tmp);
-    free(tmp);
-    return (1);
+	tmp = ft_strdup("cd: ");
+	if (name) 
+		tmp = ft_strrejoin(tmp, name);
+	//ft_putstr_fd("cd", 2);
+	if (en == 1)
+		tmp = ft_strrejoin(tmp, ": string not in pwd");
+	else if (en == 2)
+		tmp = ft_strrejoin(tmp, ": no such file or directory");
+	else if (en == 3)
+		tmp = ft_strrejoin(tmp, ": permission denied");
+	else if (en == 4)
+		tmp = ft_strrejoin(tmp, ": not a directory");
+	else if (en == 5)
+		tmp = ft_strrejoin(tmp, "too many arguments");
+	else if (en == 6)
+		tmp = ft_strrejoin(tmp, "OLDPWD not set");
+	error_handler(VARIABLE_ERROR | (ERR_CD << 9), tmp);
+	free(tmp);
+	return (1);
 }
 
-int     btin_cd(t_ltree *pos)
+int         btin_cd(t_ltree *pos)
 {
-    int     i;
-    t_cd    *flags;
+	int     i;
+	t_cd    *flags;
 
-    flags = ft_xmalloc(sizeof(t_cd *));
-    i = ft_cd_flags(pos->ar_v, flags);
-    if (ft_valid_cd(pos->ar_v, i))
-    {
-        free(flags->curpath);
-        free(flags);
-        return (1);
-    }
-    if (ft_cd_pars(pos->ar_v[i], g_env, flags))
-    {
-        free(flags->curpath);
-        free(flags);
-        return (1);
-    }
-        free(flags->curpath);
-    free(flags);
-    return (0);
+	flags = ft_xmalloc(sizeof(t_cd *));
+	i = ft_cd_flags(pos->ar_v, flags);
+	if (ft_valid_cd(pos->ar_v, i))
+	{
+		free(flags->curpath);
+		free(flags);
+		return (1);
+	}
+	if (ft_cd_pars(pos->ar_v[i], g_env, flags))
+	{
+		free(flags->curpath);
+		free(flags);
+		return (1);
+	}
+		free(flags->curpath);
+	free(flags);
+	return (0);
 }
