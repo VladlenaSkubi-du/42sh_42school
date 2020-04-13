83b20a554d98fe695417181718c485d2a83d11fc
rbednar-vs <rabota.voyt@mail.ru>
2020-04-13 12:42:45 +0300

    add error if can not create tmpfile

--- srcs/parser/substitution/substitution.c
+++ srcs/parser/substitution/substitution.c
@@ -43,7 +43,7 @@ int		before_add(t_ltree *sub, t_list **list)
 	pre_parsing_cut_glue(sub);
 	if ((err = ft_find_redirection(sub)) & ERR_OUT)
 	{
-		if ((err & 0x1FF) != TMPFILE)
+		if ((err & 0xFF) != TMPFILE)
 			ft_error_redir(sub);
 		else
 			error_handler(err, NULL);
