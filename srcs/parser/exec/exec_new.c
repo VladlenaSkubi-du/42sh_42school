1fda9abcb6cae3373644758f3241946e1071721d
Skeevert <iceknight48@gmail.com>
2020-04-28 15:55:32 +0300

    I tried so hard and got so far, but in the end it doesn't even matter

--- srcs/parser/exec/exec_new.c
+++ srcs/parser/exec/exec_new.c
@@ -16,50 +16,40 @@ int		exec_vp(process *p)
 	return (0);
 }
 
+void	set_proc_sig(void)
+{
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+	signal(SIGTSTP, SIG_DFL);
+	signal(SIGTTIN, SIG_DFL);
+	signal(SIGTTOU, SIG_DFL);
+	signal(SIGCHLD, SIG_DFL);
+}
+
+int		setstream(int src, int dest)
+{
+	dup2(src, dest);
+	close(dest);
+	return (0);
+}
+
 void	launch_process (process *p, pid_t pgid, int stream[3], int foreground)
 {
 	pid_t pid;
 
-
  	if (g_is_interactive)
 	{
-      /* Put the process into the process group and give the process group
-         the terminal, if appropriate.
-         This has to be done both by the shell and in the individual
-         child processes because of potential race conditions.  */
 		pid = getpid ();
 		if (pgid == 0) pgid = pid;
 		setpgid (pid, pgid);
-
 		signal(SIGTTOU, SIG_IGN);
 		if (foreground)
 			tcsetpgrp(STDIN_FILENO, pgid);
-		/* Set the handling for job control signals back to the default.  */
-		signal(SIGINT, SIG_DFL);
-		signal(SIGQUIT, SIG_DFL);
-		signal(SIGTSTP, SIG_DFL);
-		signal(SIGTTIN, SIG_DFL);
-		signal(SIGTTOU, SIG_DFL);
-		signal(SIGCHLD, SIG_DFL);
+		set_proc_sig();
 	}
-
-  /* Set the standard input/output channels of the new process.  */
-	if (stream[0] != STDIN_FILENO)
-	{
-		dup2 (stream[0], STDIN_FILENO);
-		close (stream[0]);
-	}
-	if (stream[1] != STDOUT_FILENO)
-	{
-		dup2 (stream[1], STDOUT_FILENO);
-		close (stream[1]);
-	}
-	if (stream[2] != STDERR_FILENO)
-	{
-		dup2 (stream[2], STDERR_FILENO);
-		close (stream[2]);
-	}
-	/* Exec the new process.  Make sure we exit.  */
+	(stream[0] != STDIN_FILENO) && setstream(stream[0], STDIN_FILENO);
+	(stream[1] != STDOUT_FILENO) && setstream(stream[1], STDOUT_FILENO);
+	(stream[2] != STDERR_FILENO) && setstream(stream[2], STDERR_FILENO);
 	exec_vp(p);
 	exec_clean(g_path, 0, 0);
 	exit(1);
