d006188ccb6c8140650c54e2c97e0decd6863880
Skeevert <iceknight48@gmail.com>
2020-05-04 14:42:53 +0300

    Jobs builtin started options parse

--- srcs/builtin/jobs_btin.c
+++ srcs/builtin/jobs_btin.c
@@ -2,7 +2,20 @@
 #include "builtin42.h"
 #include "jobs.h"
 
-/*
+int		id_check(t_ltree *pos)
+{
+	int		i;
+
+	i = 1;
+	while (i < pos->ar_c)
+	{
+		if (ft_atoi(pos->ar_v[i]) > 0)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 int		options_parse(t_ltree *pos)
 {
 	int		ret;
@@ -16,17 +29,23 @@ int		options_parse(t_ltree *pos)
 		if (pos->ar_v[i][0] == '-')
 		{
 			j = 1;
-			if (!(ret | 0x04) && (pos->ar_v[i][j] == 'l' ||
-				pos->ar_v[i][j] == 'p' || (!pos->ar_v[i][j])))
+			while (pos->ar_v[i][j])
 			{
-				pos->ar_v[i][1] ==
+				if (pos->ar_v[i][j] == 'p')
+					ret = 1;
+				else if (pos->ar_v[i][j] == 'l')
+					ret = 2;
+				else
+					return (-1);
+				j++;
 			}
 		}
 		i++;
 	}
+	return (ret);
 }
-*/
-int		get_status(job	*j, char *buff)
+
+int		get_status(job	*j, char *buff, int options)
 {
 	process		*p_iter;
 	int			signum;
@@ -40,9 +59,9 @@ int		get_status(job	*j, char *buff)
 		if (!p_iter)
 			return (-1);
 		signum = WSTOPSIG(p_iter->status);
-		if (signum == SIGTTIN)
+		if (signum == SIGTTIN && options == 2)
 			ft_strcpy(buff, "stopped (tty input) ");
-		else if (signum == SIGTTOU)
+		else if (signum == SIGTTOU && options == 2)
 			ft_strcpy(buff, "stopped (tty output)");
 		else
 			ft_strcpy(buff, "stopped             ");
@@ -52,7 +71,7 @@ int		get_status(job	*j, char *buff)
 	return (0);
 }
 
-int		print_job_info(job * j)
+int		print_job_info(job * j, int options)
 {
 	char	def;
 	char	status[100];
@@ -63,24 +82,44 @@ int		print_job_info(job * j)
 		def = '-';
 	else
 		def = ' ';
-	if (get_status(j, status))
+	if (get_status(j, status, options))
 		return (-1);
-	printf("[%d]%c %d %s %s\n", j->jid, def, j->pgid, status, j->com);
+	options != 1 && ft_printf("[%d]%c ", j->jid, def);
+	ft_printf("%d", j->pgid);
+	options != 1 && ft_printf(" %s %s", status, j->com);
+	ft_printf("\n");
 	return (0);
 }
 
 int		btin_jobs(t_ltree *pos) /* TODO: PROCESS OPTIONS! */
 {
 	job		*job_iter;
-//	int		options;
+	int		options;
+	int		id_chk;
+	int		i;
+	int		id;
 
+	i = 1;
 	job_iter = g_first_job;
 	if (!job_iter)
 		return (0);
-	while (job_iter)
+	options = options_parse(pos);
+	if (options == -1)
+		return (-1);
+	id_chk = id_check(pos);
+	while (!id_chk && job_iter)
 	{
-		print_job_info(job_iter);
+		print_job_info(job_iter, options);
 		job_iter = job_iter->next;
+	}
+	while (id_chk && i < pos->ar_c)
+	{
+		id = ft_atoi((pos->ar_v)[i]);
+		job_iter = g_first_job;
+		while (job_iter && job_iter->jid != id)
+			job_iter = job_iter->next;
+		job_iter && print_job_info(job_iter, options);
+		i++;
 	}
 	return (0);
 }
