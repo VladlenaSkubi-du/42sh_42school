b973b724905dbf4c00fe4b017b6aa6eee219f35b
Владлена Скубиш <vladlenaskubis@MacBook-AirVlada.local>
2020-07-24 10:32:12 +0300

    Enter for type

--- srcs/builtin/hash/hashtable_from_type.c
+++ srcs/builtin/hash/hashtable_from_type.c
@@ -1,22 +1,49 @@
 #include "shell42.h"
 #include "hash.h"
 
-char		*hashtable_is_hashed(int *where, char *key)
+/*
+** Returns NULL or full path to a key (command)
+** returns NULL is command is not found in hashtable and after
+** PATH parsing
+** returns path is the command is hashed (where changes to 1)
+** or if command is not hashed but found in PATH parsing - only
+** if it is executable (where is changed to 0)
+*/
+
+char		*hashtable_type_init(int *where, char *key)
 {
 	char	*path;
-
-	path = NULL;
-	if (ft_strcmp(key, "new") == 0)
-		*where = 0;
-	else
-		*where = 1;
+	void	**hashtable;
+	int		hashtable_size;
+	int		index;
 	
-	// if (path == NULL)
-	// 	return (path);
-	// else if (access(path, X_OK) == -1)
-	// {
-	// 	free(path);
-	// 	return (NULL);
-	//}
+	path = NULL;
+	hashtable_size = -1;
+	hashtable = get_hashtable_value(&hashtable_size);
+	if (hashtable_size < 0)
+	{
+		printf("can's get hashtable value\n");
+		return (NULL);
+	}
+	index = hashtable_find(key, hashtable, hashtable_size);
+	if (index == HASHTABLE_NF)
+	{
+		path = hashtable_key_nf_type(key, hashtable, hashtable_size);
+		*where = 0;
+		return ((path == NULL) ? NULL : path);
+	}
+	hashtable_check_valid_type(index, key, hashtable, hashtable_size);
+	return (path);
+}
+
+char		*hashtable_key_nf_type(char *key, void **hashtable,
+				int hashtable_size)
+{
 	return (NULL);
+}
+
+int			hashtable_check_valid_type(int index, char *key,
+				void **hashtable, int hashtable_size)
+{
+	return (0);
 }
