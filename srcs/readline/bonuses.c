35369b757adaf221ab1c638a0b999176ea333fac
Sincere Schmeler <sschmele@am-d1.msk.21-school.ru>
2020-08-18 20:35:46 +0300

    Exec_old deleted and bonuses added

--- srcs/readline/bonuses.c
+++ srcs/readline/bonuses.c
@@ -1,23 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   easter_egg.c                                       :+:      :+:    :+:   */
+/*   bonuses.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sschmele <sschmele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/25 18:47:38 by sschmele          #+#    #+#             */
-/*   Updated: 2020/07/25 18:47:39 by sschmele         ###   ########.fr       */
+/*   Created: 2020/07/25 18:52:56 by sschmele          #+#    #+#             */
+/*   Updated: 2020/08/01 17:51:39 by sschmele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell42.h"
 #include "readline.h"
 
-int				kirill_lgbt(void)
+#define COLOR_NUM	6
+
+char				*colors_process(int sequence_num)
 {
-	int			i;
-	char		*color;
-	char		*kirill;
+	char			*seq_action[COLOR_NUM];
+
+	seq_action[0] = RED;
+	seq_action[1] = ORANGE;
+	seq_action[2] = YELLOW;
+	seq_action[3] = GREEN;
+	seq_action[4] = BLUE;
+	seq_action[5] = PURPLE;
+	return (seq_action[sequence_num]);
+}
+
+int					kirill_lgbt(void)
+{
+	int				i;
+	char			*color;
+	char			*kirill;
 
 	kirill = ft_strstr(g_rline.cmd, "kirill");
 	if (kirill != NULL && g_rline.pos > 5 &&
