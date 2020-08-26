18f6fecadf0fb52e4bb24b705244f868ee78ef67
Владлена Скубиш <vladlenaskubis@MacBook-AirVlada.local>
2020-08-26 23:16:42 +0300

    Fix of segfault if there is no exit because of the errors but g_rline.cmd is freed

--- srcs/builtin/exit.c
+++ srcs/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: sschmele <sschmele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/21 16:27:36 by sschmele          #+#    #+#             */
-/*   Updated: 2020/08/25 22:17:37 by sschmele         ###   ########.fr       */
+/*   Updated: 2020/08/26 23:11:20 by sschmele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,27 +30,31 @@ int				btin_exit(t_process *pos)
 		job_iter = job_iter->next;
 	}
 	if (pos->argc > 2)
-	{
-		error_handler(VARIABLE_ERROR | (ERR_TOO_MANY << 9), "exit");
-		return (BTIN_ERROR);
-	}
+		return (btin_exit_error(NULL, VARIABLE_ERROR));
 	status = 0;
 	if (pos->argc > 1 && (status = btin_exit_args(pos->argv)) == BTIN_ERROR)
 		return (BTIN_ERROR);
+	free(g_rline.cmd);
+	g_rline.cmd = NULL;
 	ft_putendl_fd("exit", STDOUT_FILENO);
 	fill_hist_in_file();
 	clean_everything();
 	exit(status);
 }
 
-int				btin_exit_error(char *option)
+int				btin_exit_error(char *option, int error)
 {
 	char	*error_message;
 
-	error_message = ft_strjoin("exit: ", option);
-	error_handler(SYNTAX_ERROR | (ERR_NUMERIC << 9), error_message);
-	usage_btin("exit");
-	free(error_message);
+	if (error == SYNTAX_ERROR)
+	{
+		error_message = ft_strjoin("exit: ", option);
+		error_handler(SYNTAX_ERROR | (ERR_NUMERIC << 9), error_message);
+		usage_btin("exit");
+		free(error_message);
+	}
+	else if (error == VARIABLE_ERROR)
+		error_handler(VARIABLE_ERROR | (ERR_TOO_MANY << 9), "exit");
 	return (BTIN_ERROR);
 }
 
@@ -65,7 +69,7 @@ int				btin_exit_args(char **ar_v)
 	i = (ar_v[1][0] && ar_v[1][0] == '-') ? 0 : -1;
 	while (ar_v[1][++i])
 		if (!(ar_v[1][i] >= '0' && ar_v[1][i] <= '9'))
-			return (btin_exit_error(ar_v[1]));
+			return (btin_exit_error(ar_v[1], SYNTAX_ERROR));
 	status = ft_atoi(ar_v[1]);
 	(status < 0) ? status &= 0xFF : 0;
 	return (status);
