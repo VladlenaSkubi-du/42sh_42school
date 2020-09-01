72bef77bc44378327dcda7b9119ca5dccd86f6cd
VladlenaSkubi-du <vskubish1410@bk.ru>
2020-09-01 10:34:23 +0300

    changes in scripts

--- scripts/expansion_srcipt.sh
+++ scripts/expansion_srcipt.sh
@@ -1,76 +1,75 @@
 #! /bin/bash
 
 TEST='|U hate this|'
-echo -e "\033[0;32m*** Simple tests ***\033[0m"
-echo -e "\033[1;34m~~~~~~~~~~~~1~~~~~~~~~~~~\033[0m"
-echo ${TEST}
-echo -e "\033[0;31m|U hate this|\033[0m"
-echo -e "\033[1;34m~~~~~~~~~~~~2~~~~~~~~~~~~\033[0m"
-echo ${TEST}$TEST
-echo -e "\033[0;31m|U hate this||U hate this|\033[0m"
-echo -e "\033[1;34m~~~~~~~~~~~~3~~~~~~~~~~~~\033[0m"
-echo ${TES}$TEST
-echo -e "\033[0;31m|U hate this|\033[0m"
-echo -e "\033[1;34m~~~~~~~~~~~~4~~~~~~~~~~~~\033[0m"
-echo $TEST${TEST}
-echo -e "\033[0;31m|U hate this||U hate this|\033[0m"
-echo -e "\033[1;34m~~~~~~~~~~~~5~~~~~~~~~~~~\033[0m"
-echo ${TEST}AAAA
-echo -e "\033[0;31m|U hate this|AAAA\033[0m"
-echo -e "\033[1;34m~~~~~~~~~~~~6~~~~~~~~~~~~\033[0m"
-echo AAAA${TEST}AAAA
-echo -e "\033[0;31mAAAA|U hate this|AAAA\033[0m"
-echo -e "\033[1;34m~~~~~~~~~~~~7~~~~~~~~~~~~\033[0m"
-echo AAAA${TST}AAAA
-echo -e "\033[0;31mAAAAAAAA\033[0m"
-echo -e "\033[1;34m~~~~~~~~~~~~8~~~~~~~~~~~~\033[0m"
-echo AAAA${TEST}AAAA$TEST
-echo -e "\033[0;31mAAAA|U hate this|AAAA|U hate this|\033[0m"
-echo -e "\033[1;34m~~~~~~~~~~~~9~~~~~~~~~~~~\033[0m"
-echo $TESTasdf
-echo -e "\033[0;31m\033[0m"
-echo -e "\033[1;34m~~~~~~~~~~~~10~~~~~~~~~~~\033[0m"
-echo AAA$TEST
-echo -e "\033[0;31mAAA|U hate this|\033[0m"
-echo -e "\033[1;34m~~~~~~~~~~~~11~~~~~~~~~~~\033[0m"
-echo AAA$TET
-echo -e "\033[0;31mAAA\033[0m"
-echo -e "\033[1;34m~~~~~~~~~~~~12~~~~~~~~~~~\033[0m"
-echo $$
-echo -e "\033[1;34m~~~~~~~~~~~~13~~~~~~~~~~~\033[0m"
-echo $$$TEST
-echo -e "\033[1;34m~~~~~~~~~~~~13~~~~~~~~~~~\033[0m"
-echo $$$$TEST
-echo -e "\033[1;34m~~~~~~~~~~~~13~~~~~~~~~~~\033[0m"
+printf "\033[0;32m*** Simple tests ***\033[0m\n"
+printf "\033[1;34m~~~~~~~~~~~~1~~~~~~~~~~~~\033[0m\n"
+TEST='|U hate this|' echo ${TEST}
+printf "\033[0;31m|U hate this|\033[0m\n"
+printf "\033[1;34m~~~~~~~~~~~~2~~~~~~~~~~~~\033[0m\n"
+TEST='|U hate this|' echo ${TEST}$TEST
+printf "\033[0;31m|U hate this||U hate this|\033[0m\n"
+printf "\033[1;34m~~~~~~~~~~~~3~~~~~~~~~~~~\033[0m\n"
+TEST='|U hate this|' echo ${TES}$TEST
+printf "\033[0;31m|U hate this|\033[0m\n"
+printf "\033[1;34m~~~~~~~~~~~~4~~~~~~~~~~~~\033[0m\n"
+TEST='|U hate this|' echo $TEST${TEST}
+printf "\033[0;31m|U hate this||U hate this|\033[0m\n"
+printf "\033[1;34m~~~~~~~~~~~~5~~~~~~~~~~~~\033[0m\n"
+TEST='|U hate this|' echo ${TEST}AAAA
+printf "\033[0;31m|U hate this|AAAA\033[0m\n"
+printf "\033[1;34m~~~~~~~~~~~~6~~~~~~~~~~~~\033[0m\n"
+TEST='|U hate this|' echo AAAA${TEST}AAAA
+printf "\033[0;31mAAAA|U hate this|AAAA\033[0m\n"
+printf "\033[1;34m~~~~~~~~~~~~7~~~~~~~~~~~~\033[0m\n"
+TEST='|U hate this|' echo AAAA${TST}AAAA
+printf "\033[0;31mAAAAAAAA\033[0m\n"
+printf "\033[1;34m~~~~~~~~~~~~8~~~~~~~~~~~~\033[0m\n"
+TEST='|U hate this|' echo AAAA${TEST}AAAA$TEST
+printf "\033[0;31mAAAA|U hate this|AAAA|U hate this|\033[0m\n"
+printf "\033[1;34m~~~~~~~~~~~~9~~~~~~~~~~~~\033[0m\n"
+TEST='|U hate this|' echo $TESTasdf
+printf "\033[0;31m\033[0m\n"
+printf "\033[1;34m~~~~~~~~~~~~10~~~~~~~~~~~\033[0m\n"
+TEST='|U hate this|' echo AAA$TEST
+printf "\033[0;31mAAA|U hate this|\033[0m\n"
+printf "\033[1;34m~~~~~~~~~~~~11~~~~~~~~~~~\033[0m\n"
+TEST='|U hate this|' echo AAA$TET
+printf "\033[0;31mAAA\033[0m\n"
+printf "\033[1;34m~~~~~~~~~~~~12~~~~~~~~~~~\033[0m\n"
+TEST='|U hate this|' echo $$
+printf "\033[1;34m~~~~~~~~~~~~13~~~~~~~~~~~\033[0m\n"
+TEST='|U hate this|' echo $$$TEST
+printf "\033[1;34m~~~~~~~~~~~~13~~~~~~~~~~~\033[0m\n"
+TEST='|U hate this|' echo $$$$TEST
+printf "\033[1;34m~~~~~~~~~~~~13~~~~~~~~~~~\033[0m\n"
 
-echo -e "\033[0;32m*** Middle tests ***\033[0m"
+printf "\033[0;32m*** Middle tests ***\033[0m\n"
 
 TEST2='|JTERRY|'
 TEST1='|GGWIN-GO|'
-echo -e "\033[1;34m~~~~~~~~~~~~1~~~~~~~~~~~~\033[0m"
+printf "\033[1;34m~~~~~~~~~~~~1~~~~~~~~~~~~\033[0m\n"
 echo ${TEST2:-$TEST1}
-echo -e "\033[0;31m|JTERRY|\033[0m"
-echo -e "\033[1;34m~~~~~~~~~~~~2~~~~~~~~~~~~\033[0m"
+printf "\033[0;31m|JTERRY|\033[0m\n"
+printf "\033[1;34m~~~~~~~~~~~~2~~~~~~~~~~~~\033[0m\n"
 echo ${TES2:-$TEST1}
-echo -e "\033[0;31m|GGWIN-GO|\033[0m"
-echo -e "\033[1;34m~~~~~~~~~~~~3~~~~~~~~~~~~\033[0m"
+printf "\033[0;31m|GGWIN-GO|\033[0m\n"
+printf "\033[1;34m~~~~~~~~~~~~3~~~~~~~~~~~~\033[0m\n"
 echo ${TES2:-$TES1}
-echo -e "\033[0;31m\033[0m"
-echo -e "\033[1;34m~~~~~~~~~~~~4~~~~~~~~~~~~\033[0m"
+printf "\033[0;31m\033[0m\n"
+printf "\033[1;34m~~~~~~~~~~~~4~~~~~~~~~~~~\033[0m\n"
 echo ${TEST2:+$TEST1}
-echo -e "\033[0;31m|GGWIN-GO|\033[0m"
-echo -e "\033[1;34m~~~~~~~~~~~~5~~~~~~~~~~~~\033[0m"
+printf "\033[0;31m|GGWIN-GO|\033[0m\n"
+printf "\033[1;34m~~~~~~~~~~~~5~~~~~~~~~~~~\033[0m\n"
 echo ${TET2:+$TEST1}
-echo -e "\033[0;31m\033[0m"
-echo -e "\033[1;34m~~~~~~~~~~~~6~~~~~~~~~~~~\033[0m"
+printf "\033[0;31m\033[0m\n"
+printf "\033[1;34m~~~~~~~~~~~~6~~~~~~~~~~~~\033[0m\n"
 echo ${TEST2:=$TEST1}
-echo -e "\033[0;31m|JTERRY|\033[0m"
-echo -e "\033[1;34m~~~~~~~~~~~~6~~~~~~~~~~~~\033[0m"
+printf "\033[0;31m|JTERRY|\033[0m\n"
+printf "\033[1;34m~~~~~~~~~~~~6~~~~~~~~~~~~\033[0m\n"
 echo ${TET2:=$TEST1}
-echo -e "\033[0;31m|GGWIN-GO|\033[0m"
-echo -e "\033[1;34m~~~~~~~~~~~~7~~~~~~~~~~~~\033[0m"
+printf "\033[0;31m|GGWIN-GO|\033[0m\n"
+printf "\033[1;34m~~~~~~~~~~~~7~~~~~~~~~~~~\033[0m\n"
 echo ${TET2}
-echo -e "\033[0;31m|GGWIN-GO|\033[0m"
-echo -e "\033[1;34m~~~~~~~~~~~~8~~~~~~~~~~~~\033[0m"
-echo ${PWD:PWD} 
-echo -e "\033[0;31m42sh: unrecognized modifier \`P\'\033[0m"
+printf "\033[0;31m|GGWIN-GO|\033[0m\n"
+printf "\033[1;34m~~~~~~~~~~~~8~~~~~~~~~~~~\033[0m\n"
+echo ${PWD:PWD}
--- scripts/job_control.sh
+++ scripts/job_control.sh
@@ -1,12 +1,12 @@
 BLUE='\033[0;34m'
 RED='\033[0;34m'
-GREEN='$GREENm'
-NC='$NC'
+GREEN='\033[0;32m'
+NC='\033[0m'
 
 DEFAULT_DIR=$PWD
 
-printf "\n$GREENm# Job control. $NC"
-printf "\n$BLUEm# Message indicating that the "ls" command is being executed $NC \n"
+printf "\n\033[0;32m# Job control. \033[0m"
+printf "\n\033[0;34m# Message indicating that the "ls" command is being executed \033[0m \n"
 cd /tmp
 mkfifo fifo
 ls -lR /usr >fifo 2>&1 &
@@ -16,7 +16,7 @@ rm fifo
 jobs
 sleep 1
 
-printf "\n$BLUEm# Message indicating that 5 instances of emacs are stopped in the background $NC \n"
+printf "\n\033[0;34m# Message indicating that 5 instances of emacs are stopped in the background \033[0m \n"
 emacs -nw &
 sleep 1
 emacs -nw &
@@ -30,7 +30,7 @@ sleep 1
 jobs
 sleep 1
 
-printf "\n$BLUEm# The "ls" command launched above must be the only one left $NC \n"
+printf "\n\033[0;34m# The "ls" command launched above must be the only one left \033[0m \n"
 kill -9 %
 sleep 1
 kill -9 %
@@ -46,13 +46,13 @@ sleep 1
 kill -9 %
 sleep 1
 
-printf "\n$BLUEm# Output of the "ls | cat -e" command $NC \n"
+printf "\n\033[0;34m# Output of the "ls | cat -e" command \033[0m \n"
 ls | cat -e &
 sleep 1
 jobs
 sleep 1
 
-printf "\n$BLUEm# Message indicating that the order is suspended $NC \n"
+printf "\n\033[0;34m# Message indicating that the order is suspended \033[0m \n"
 ls -Rl / 2>&1 &
 sleep 1
 kill -s SIGTSTP %
@@ -60,17 +60,17 @@ sleep 1
 jobs
 sleep 1
 
-printf "\n$BLUEm# PID of the command "ls" from the previous test $NC \n"
+printf "\n\033[0;34m# PID of the command "ls" from the previous test \033[0m \n"
 ps a | grep "ls -Rl /" | grep -v 'grep' | cut -d ' ' -f 2
 sleep 1
 
-printf "\n$BLUEm# Message indicating that the order has ended $NC \n"
+printf "\n\033[0;34m# Message indicating that the order has ended \033[0m \n"
 kill -9 %
 sleep 1
 jobs
 sleep 1
 
-printf "\n$BLUEm# Output should display nothing for (ps a | grep \"ls -Rl /\" | grep -v 'grep') $NC \n"
+printf "\n\033[0;34m# Output should display nothing for (ps a | grep \"ls -Rl /\" | grep -v 'grep') \033[0m \n"
 ps a | grep "ls -Rl /" | grep -v 'grep'
 sleep 1
 
